36db1603b5421a2e1299ca92a3931d20b113aa49
Michel van den berg <michel.van.den.berg@collenda.com>
2020-12-22 13:26:51 +0100

    Initial version

---
+++ LeakyApp/Clock.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace LeakyApp
+{
+    public partial class Clock : Window
+    {
+        DispatcherTimer timer;
+
+        public Clock()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+
+            timer.Start();
+            timer.Tick += UpdateTime;
+        }
+
+        private void UpdateTime(object sender, EventArgs e)
+        {
+            timerText.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            //Uncommnet below lines to stop memory leak
+
+            //timer.Tick -= UpdateTime;
+            //timer.Stop();
+        }
+    }
+}
---
+++ LeakyApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace LeakyApp
+{
+    public partial class MainWindow : Window
+    {
+        List<Clock> clocks = new List<Clock>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                var clock = new Clock();
+                clock.Show();
+                clocks.Add(clock);
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var clock in clocks)
+            {
+                clock.Close();
+            }
+            clocks.Clear();
+        }
+    }
+}
